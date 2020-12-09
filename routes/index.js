a16db9fa47add76efb8f65bd8ae16e93d7219e76
raj14ar <raj14ar@gmail.com>
2020-12-10 00:41:26 +0530

    Add user post controller

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ console.log("Router loaded");
 const homeController = require('../controllers/home_controller');
 
 router.get('/',homeController.home);
+router.use('/users', require('./users'));
 
 module.exports = router;
