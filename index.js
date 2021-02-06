dc0f3c1220aa000ea1fd197ee088096dedde43b8
raj14ar <raj14ar@gmail.com>
2021-02-06 12:54:49 +0530

    Setup mongo store to store session cookie to prevent deletion of cookie after server restart

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require("./config/passport-local-strategy");
-
+const MongoStore = require('connect-mongo')(session);
 
 app.use(express.urlencoded());
 //setup cookie parser
@@ -26,7 +26,7 @@ app.set('layout extractScripts',true);
 app.set('view engine', 'ejs');
 app.set('views','./views');
 
-
+// mongo store is used to store the session cookie in db
 app.use(session({
     name: 'codeial',
     //TODO change the secret before deployment in production mode
@@ -35,7 +35,16 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemoved: 'disabled'
+        },
+        function(err){
+            console.log(err|| 'connect-mongodb setup ok');
+        }
+    )
 }));
 
 app.use(passport.initialize());
